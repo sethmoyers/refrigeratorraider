3870262a7abcef51d247a5da0f3d02d0b5fd5bda
baustin <bryant.austin@gmail.com>
2017-02-24 21:36:50 -0500

    Bug Fixes
    - Fixed bug with quantity field accepting letters instead of numbers.
    
    - other spelling errors in alerts

--- assets/js/cs633.js
+++ assets/js/cs633.js
@@ -4,53 +4,11 @@
 // *** except the LOGON Menu item
 // ***
 // window.onload = function() {  // execute on form load
-window.onload = function() {  // execute on form load {
-	// ** Added: 02-23-2017 B. Austin
-	// ** Validate Quantity Fields are Numbers Only
-	// **
-	console.log( "Start of Binding Code" );
-	var thor = $(".onlyNumbers");
-	console.log("***THOR*** ===> " + thor);
-	thor.bind("keydown",function(event){
-		// the keycode for the key pressed 
-		var keyCode = event.which;
-		console.log( "Binding One" );
-		// 48-57 Standard Keyboard Numbers
-		var isStandard = (keyCode > 47 && keyCode < 58);
-		
-		// 96-105 Extended Keyboard Numbers (aka Keypad) 
-		var isExtended = (keyCode > 95 && keyCode < 106);
-		
-		// 8 Backspace, 46 Forward Delete
-		// 37 Left Arrow, 38 Up Arrow, 39 Right Arrow, 40 Down Arrow
-		var validKeyCodes = ',8,37,38,39,40,46,';
-		var isOther = (validKeyCodes.indexOf(',' + keyCode + ',') > -1);
-
-		if (isStandard || isExtended || isOther) {
-			console.log( "Handler for .keydown() called." );
-			return true;
-		} else {
-			console.log( "Handler for .keydown() NOT called." );
-			return false;
-		}
-	}).bind('blur',function(){
-		// regular expression that matches everything that is not a number 
-		var pattern = new RegExp('[^0-9]+', 'g');
-		
-		var $input = $(this);
-		var value = $input.val();
-		console.log( "Binding Two" );
-		// clean the value using the regular expression 
-		value = value.replace(pattern, '');
-		$input.val(value);
-	});
-console.log( "Leaving Module" );
-
+$(document).ready(function() {  // execute on form load {
 	$("#viewsupplier_menu").hide();
 	$("#about_menu").hide();
 	$("#help_menu").hide();
 	$("#fridgeraider_menu").hide();
 	$("#fridgeowner_menu").hide();
 	$("#shopping_menu").hide();
-
-};
+});
